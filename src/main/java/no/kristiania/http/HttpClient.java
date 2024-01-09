583447b2032a757f86623311d16f2df81ecf5972
Mikael Gãrtner <Mikael_gartner@hotmail.com>
2024-01-09 16:18:20 +0100

    HttpServer shouldReturn404ForUnknownRequestTarget (red)

--- src/main/java/no/kristiania/http/HttpClient.java
+++ src/main/java/no/kristiania/http/HttpClient.java
@@ -43,7 +43,7 @@ public class HttpClient {
         return buffer.toString();
     }
 
-    private String readLine(Socket socket) throws IOException {
+    static String readLine(Socket socket) throws IOException {
         StringBuilder buffer = new StringBuilder();
         int c;
         while ((c = socket.getInputStream().read()) != '\r') {
---
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -0,0 +1,49 @@
+package no.kristiania.http;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServer {
+
+    private final ServerSocket serverSocket;
+
+    public HttpServer(int serverPort) throws IOException {
+        serverSocket = new ServerSocket(serverPort);
+
+        new Thread(this::handleClients).start();
+    }
+
+    private void handleClients() {
+        try {
+            Socket clientSocket = serverSocket.accept();
+            String response = "HTTP/1.1 404 Not found\r\n\r\n";
+            clientSocket.getOutputStream().write(response.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(1962);
+
+        Socket clientSocket = serverSocket.accept();
+
+        String html = "Hallo der";
+        String contentType = "text/html";
+
+        String response = "HTTP/1.1 200 OK\r\n" +
+                "Content-Length: " + (html.length()) + "\r\n" + //Her kunne man brukt html.getBytes().length for ekstrapoeng
+                "Content-Type: " + contentType + "\r\n" +
+                "Connection: close\r\n" +
+                "\r\n" +
+                html;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+
+
+    }
+
+}
+
